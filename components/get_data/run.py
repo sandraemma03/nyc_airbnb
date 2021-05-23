ab8c3fede36e45d24110fc2e9c9e07d793b73b85
sandraemma03 <sanraemma03@gmail.com>
2021-05-23 17:00:43 +0100

    fixed error

--- components/get_data/run.py
+++ components/get_data/run.py
@@ -14,6 +14,33 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
 
+
+# def log_artifact(artifact_name, artifact_type, artifact_description, filename, wandb_run):
+#     """
+#     Log the provided filename as an artifact in W&B, and add the artifact path to the MLFlow run
+#     so it can be retrieved by subsequent steps in a pipeline
+
+#     :param artifact_name: name for the artifact
+#     :param artifact_type: type for the artifact (just a string like "raw_data", "clean_data" and so on)
+#     :param artifact_description: a brief description of the artifact
+#     :param filename: local filename for the artifact
+#     :param wandb_run: current Weights & Biases run
+#     :return: None
+#     """
+#     # Log to W&B
+#     artifact = wandb.Artifact(
+#         artifact_name,
+#         type=artifact_type,
+#         description=artifact_description,
+#     )
+#     artifact.add_file(filename)
+#     wandb_run.log_artifact(artifact)
+#     # We need to call this .wait() method before we can use the
+#     # version below. This will wait until the artifact is loaded into W&B and a
+#     # version is assigned
+#     artifact.wait()
+
+
 def go(args):
     run = wandb.init(job_type="download_file")
     run.config.update(args)
@@ -25,32 +52,12 @@ def go(args):
         type=args.artifact_type,
         description=args.artifact_description,
     )
-    artifact.add_file(os.path.join("data", args.sample))
-
-     # than the available memory
     logger.info(f"Returning sample {args.sample}")
     logger.info(f"Uploading {args.artifact_name} to Weights & Biases")
     
+
+    artifact.add_file(os.path.join("data", args.sample))
     run.log_artifact(artifact)
-
-
-
-# def go(args):
-
-#     run = wandb.init(job_type="download_file")
-#     run.config.update(args)
-
-#     # We stream the file so that it can be downloaded even if it is bigger
-#     # than the available memory
-#     logger.info(f"Returning sample {args.sample}")
-#     logger.info(f"Uploading {args.artifact_name} to Weights & Biases")
-#     log_artifact(
-#         args.artifact_name,
-#         args.artifact_type,
-#         args.artifact_description,
-#         os.path.join("data", args.sample),
-#         run,
-#     )
 
 
 if __name__ == "__main__":
